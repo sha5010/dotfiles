807d0c4c8a31d7256648c0e9ffc9e0c51eea0fa7
SHA-5010 <95682647+sha5010@users.noreply.github.com>
2022-01-05 11:01:20 +0900

    [update] disable filetypes in trim plugin
    - Trim.nvim で無効化するファイルタイプを追加
    - コメントの追加等

--- nvim/after/plugin/trim.rc.lua
+++ nvim/after/plugin/trim.rc.lua
@@ -1,15 +1,17 @@
 local status, trim = pcall(require, "trim")
 if (not status) then return end
 
+
 -- default config
 local config = {
-  disable = {},
+  disable = {"markdown", "no ft", "text"},
   patterns = {
-    [[%s/\s\+$//e]],
-    [[%s/\($\n\s*\)\+\%$//]],
-    [[%s/\%^\n\+//]],
-    [[%s/\(\n\n\n\)\n\+/\1/]],
+    [[%s/\s\+$//e]],                -- 行末の空白文字を削除
+    [[%s/\($\n\s*\)\+\%$//]],       -- ファイル末尾の連続した空白、空行を削除
+    [[%s/\%^\n\+//]],               -- ファイル先頭の連続した空白、空行を削除
+    [[%s/\(\n\n\n\)\n\+/\1/]],      -- 3行以上連続した空行は、2行に圧縮
   },
 }
+
 
 trim.setup(config)
