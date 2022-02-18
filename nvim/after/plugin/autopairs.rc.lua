43c4d5b08514e92ebc8d026d6ef4fe693e8fff85
SHA-5010 <95682647+sha5010@users.noreply.github.com>
2022-02-18 16:01:21 +0900

    [update] (nvim) enable fast_wrap feature in nvim-autopairs

--- nvim/after/plugin/autopairs.rc.lua
+++ nvim/after/plugin/autopairs.rc.lua
@@ -3,4 +3,16 @@ if (not status) then return end
 
 autopairs.setup({
   disable_filetype = { "TelescopePrompt" },
+  check_ts = true, -- use treesitter to check for a pair
+  fast_wrap = {
+    map = '<C-q>',
+    chars = { '{', '[', '(', '"', "'", '`' },
+    pattern = string.gsub([[ [%'%"%)%>%]%)%}%,%s] ]], '%s+', ''),
+    offset = -1, -- Offset from pattern match
+    end_key = ';',
+    keys = 'asdghklqwertyuiopzxcvbnmfj',
+    check_comma = true,
+    highlight = 'Search',
+    highlight_grey = 'Comment',
+  }
 })
