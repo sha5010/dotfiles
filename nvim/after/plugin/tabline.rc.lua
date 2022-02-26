12520b6179d7b9f9ba1e9a8d838c33af489d79a3
SHA-5010 <95682647+sha5010@users.noreply.github.com>
2022-02-26 22:42:33 +0900

    [update] (nvim) put the tabline color config in the rc file

--- nvim/after/plugin/tabline.rc.lua
+++ nvim/after/plugin/tabline.rc.lua
@@ -1,6 +1,43 @@
 local status, tabline = pcall(require, "tabline")
 if (not status) then return end
 
+local clear_highlights = function(hls)
+    for _, hl in ipairs(hls) do
+        local name, _ = unpack(hl)
+        vim.cmd('highlight clear ' .. name)
+    end
+end
+
+local hl = require("tabline.highlights")
+
+-- from ~/.local/share/nvim/plugged/nvim-tabline/lua/tabline/highlights.lua
+hl.c = {
+    active_bg = hl.get_color('TabLineSel', 'bg'),
+    inactive_bg = hl.get_color('TabLineFill', 'bg'),
+    active_text = '#eeeeee',
+    inactive_text = '#7f8490',
+    active_sep = '#a89984',             -- lualine_a_normal
+    modified_active_sep = '#fe8019',    -- lualine_a_visual
+}
+
+local hls = {
+    { 'TabLineSeparatorActive',             { guifg = hl.c.active_sep,           guibg = hl.c.active_bg } },
+    { 'TabLineSeparatorInactive',           { guifg = hl.c.inactive_text,        guibg = hl.c.inactive_bg } },
+    { 'TabLineModifiedSeparatorActive',     { guifg = hl.c.modified_active_sep,  guibg = hl.c.active_bg } },
+    { 'TabLineModifiedSeparatorInactive',   { guifg = hl.c.inactive_text,        guibg = hl.c.inactive_bg } },
+    { 'TabLinePaddingActive',               { guifg = hl.c.active_bg,            guibg = hl.c.active_bg } },
+    { 'TabLinePaddingInactive',             { guifg = hl.c.inactive_bg,          guibg = hl.c.inactive_bg } },
+    { 'TabLineModifiedActive',              { guifg = hl.c.active_text,          guibg = hl.c.active_bg } },
+    { 'TabLineModifiedInactive',            { guifg = hl.c.inactive_text,        guibg = hl.c.inactive_bg } },
+    { 'TabLineCloseActive',                 { guifg = hl.c.active_text,          guibg = hl.c.active_bg } },
+    { 'TabLineCloseInactive',               { guifg = hl.c.inactive_text,        guibg = hl.c.inactive_bg } },
+}
+
+-- re-configure highlights
+clear_highlights(hls)
+hl.highlight_all(hls)
+
+
 tabline.setup{
     no_name = '[No Name]',    -- Name for buffers with no name
     modified_icon = '',      -- Icon for showing modified buffer
@@ -13,4 +50,3 @@ tabline.setup{
     show_index = true ,       -- Shows the index of tab before filename
     show_icon = true,         -- Shows the devicon
 }
-
