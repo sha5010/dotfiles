b081cb6b9f237af67e0e74234c0e8b0f5fbc3832
SHA-5010 <95682647+sha5010@users.noreply.github.com>
2022-08-20 19:27:21 +0900

    [style] (nvim) tab to space

--- nvim/after/plugin/colorizer.rc.lua
+++ nvim/after/plugin/colorizer.rc.lua
@@ -7,13 +7,13 @@ colorizer.setup({
 }, {
   -- default_options
   RGB      = false;        -- #RGB hex codes
-	RRGGBB   = true;         -- #RRGGBB hex codes
-	names    = false;        -- "Name" codes like Blue
-	RRGGBBAA = true;         -- #RRGGBBAA hex codes
-	rgb_fn   = true;         -- CSS rgb() and rgba() functions
-	hsl_fn   = false;        -- CSS hsl() and hsla() functions
-	css      = false;        -- Enable all CSS features: rgb_fn, hsl_fn, names, RGB, RRGGBB
-	css_fn   = false;        -- Enable all CSS *functions*: rgb_fn, hsl_fn
-	-- Available modes: foreground, background
-	mode     = 'background'; -- Set the display mode.
+  RRGGBB   = true;         -- #RRGGBB hex codes
+  names    = false;        -- "Name" codes like Blue
+  RRGGBBAA = true;         -- #RRGGBBAA hex codes
+  rgb_fn   = true;         -- CSS rgb() and rgba() functions
+  hsl_fn   = false;        -- CSS hsl() and hsla() functions
+  css      = false;        -- Enable all CSS features: rgb_fn, hsl_fn, names, RGB, RRGGBB
+  css_fn   = false;        -- Enable all CSS *functions*: rgb_fn, hsl_fn
+  -- Available modes: foreground, background
+  mode     = 'background'; -- Set the display mode.
 })
