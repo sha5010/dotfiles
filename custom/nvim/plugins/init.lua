575e9197168090937bfa59d053ff85094bf967df
SHA-5010 <95682647+sha5010@users.noreply.github.com>
2022-12-06 22:39:26 +0900

    [update] (nvim) telescope config

--- custom/nvim/plugins/init.lua
+++ custom/nvim/plugins/init.lua
@@ -311,6 +311,37 @@ return {
     cond = cond_vscode,
   },
 
+  ["nvim-telescope/telescope.nvim"] = {
+    override_options = function()
+      local actions = require("telescope.actions")
+      return {
+        defaults = {
+          prompt_prefix = "   ",
+          layout_config = {
+            width = 0.90,
+            height = 0.85,
+            preview_cutoff = 100,
+          },
+          mappings = {
+            i = {
+              ["<C-[>"] = actions.close,
+              ["<C-f>"] = actions.results_scrolling_down,
+              ["<C-b>"] = actions.results_scrolling_up,
+              ["<C-j>"] = actions.move_selection_next,
+              ["<C-k>"] = actions.move_selection_previous,
+              ["<C-e>"] = actions.preview_scrolling_down,
+              ["<C-y>"] = actions.preview_scrolling_up,
+
+              -- disable default mapping
+              ["<C-u>"] = false,
+            },
+          },
+        },
+      }
+    end,
+    cond = cond_vscode,
+  },
+
   ["windwp/nvim-autopairs"] = {
     override_options = {
       disable_filetype = { "TelescopePrompt" },
