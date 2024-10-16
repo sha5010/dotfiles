4836b94b28b941d250033757dc375d9a61853de7
SHA-5010 <95682647+sha5010@users.noreply.github.com>
2024-10-16 23:02:01 +0900

    add(nvim): img-clip.nvim

--- nvim/lua/plugins/coding.lua
+++ nvim/lua/plugins/coding.lua
@@ -222,4 +222,59 @@ return {
       return opts
     end,
   },
+
+  -- handling images effectively
+  {
+    "hakonHarnes/img-clip.nvim",
+    cmd = {
+      "PasteImage",
+      "ImgClipDebug",
+      "ImgClipConfig",
+    },
+    keys = {
+      { "<leader>ip", "<cmd>PasteImage<cr>", desc = "Paste image from clipboard" },
+      {
+        "<leader>if",
+        function()
+          local telescope = require("telescope.builtin")
+          local actions = require("telescope.actions")
+          local action_state = require("telescope.actions.state")
+
+          telescope.find_files({
+            attach_mappings = function(_, map)
+              local function embed_image(prompt_bufnr)
+                local entry = action_state.get_selected_entry()
+                local filepath = entry[1]
+                actions.close(prompt_bufnr)
+
+                local img_clip = require("img-clip")
+                img_clip.paste_image(nil, filepath)
+              end
+
+              map("i", "<CR>", embed_image)
+              map("n", "<CR>", embed_image)
+
+              return true
+            end,
+            find_command = function(opts)
+              if vim.fn.executable("fd") then
+                local extensions = { "png", "jpg", "jpeg" }
+                local command = { "fd", "--type", "f", "--color", "never" }
+                for _, ext in ipairs(extensions) do
+                  table.insert(command, "-e")
+                  table.insert(command, ext)
+                end
+                return command
+              end
+              local org_find_command = opts.find_command
+              opts.find_command = nil
+              return org_find_command(opts)
+            end,
+          })
+        end,
+        desc = "Insert image from local file",
+      },
+    },
+    opts = {},
+  },
 }
